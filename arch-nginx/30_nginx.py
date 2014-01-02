c37efefa1f48544997eb992b7d1f2452540d1e32
3onyc <3onyc@x3tech.com>
2014-01-02 17:02:54 +0000

    Remove adding domain names from 30_nginx.py

--- arch-nginx/30_nginx.py
+++ arch-nginx/30_nginx.py
@@ -1,30 +1,11 @@
 #!/bin/env python2
-import os, sys, subprocess
-
-IP_ADDR = os.environ['IP_ADDR']
-ETCD_ADDR=os.environ['ETCD_ADDR']
-HIPACHE_PATH=os.environ['HIPACHE_PATH']
-WEB_HOSTNAMES=os.environ['WEB_HOSTNAME'].split('|')
-HOSTNAME=os.environ['HOSTNAME']
-
-def call(*args, **kwargs):
-    kwargs['shell'] = True
-    print subprocess.check_output(*args, **kwargs)
+import os, sys
 
 def start():
-    print " [^] Registering domain names in etcd..."
-    for web_hostname in WEB_HOSTNAMES:
-        url = "http://%s/v1/keys/%s/%s/%s" % (ETCD_ADDR, HIPACHE_PATH, web_hostname, HOSTNAME)
-        call('curl -SsL "%s" -d "value=%s:80" > /dev/null' % (url, IP_ADDR))
-
-def stop():
-    print " [v] Removing domain names from etcd..."
-    for web_hostname in WEB_HOSTNAMES:
-        url = "http://%s/v1/keys/%s/%s/%s" % (ETCD_ADDR, HIPACHE_PATH, web_hostname, HOSTNAME)
-        call('curl -SsL "%s" -X DELETE > /dev/null' % url)
+    print " [^] Ensuring log folder..."
+    if not os.path.isdir("/var/log/nginx/"):
+        os.mkdir("/var/log/nginx")
 
 if __name__ == "__main__":
     if sys.argv[1] == "start":
         start()
-    elif sys.argv[1] == "stop":
-        stop()
