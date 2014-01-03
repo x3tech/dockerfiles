58f805473d71dc8e5032221e873891767ba3ba08
3onyc <3onyc@x3tech.com>
2014-01-03 08:31:31 +0000

    Make etcd image expose 4001 and 7001 and not rely on a master

--- arch-etcd/start-etcd.sh
+++ arch-etcd/start-etcd.sh
@@ -4,4 +4,4 @@ if [ ! -d /etc/etcd ]; then
   mkdir /etc/etcd
 fi
 
-exec /usr/bin/etcd -c "$IP_ADDR:4001" -s "$IP_ADDR:7001" -n "$HOSTNAME" -C "$ETCD_MASTER_ADDR" -d /etc/etcd/
+exec /usr/bin/etcd -c "$IP_ADDR:4001" -s "$IP_ADDR:7001" -n "$HOSTNAME" -d /etc/etcd/
