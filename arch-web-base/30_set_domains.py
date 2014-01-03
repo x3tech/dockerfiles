ae5fca57a2798518bed662612650941920a42b37
3onyc <3onyc@x3tech.com>
2014-01-03 12:36:09 +0100

    Add default for HIPACHE_PATH to 30_set_domain and simplify some things

--- arch-web-base/30_set_domains.py
+++ arch-web-base/30_set_domains.py
@@ -2,11 +2,12 @@
 import os, sys, subprocess
 
 IP_ADDR = os.environ['IP_ADDR']
+HOSTNAME=os.environ['HOSTNAME']
 ETCD_ADDR=os.environ['ETCD_PORT_4001_TCP'].replace('tcp://', '')
-HIPACHE_PATH=os.environ['HIPACHE_PATH']
+HIPACHE_PATH=os.environ.get('HIPACHE_PATH', '/hipache')
 WEB_HOSTNAMES=os.environ['WEB_HOSTNAME'].split('|')
 WEB_PORT=os.environ.get('WEB_PORT', '80')
-HOSTNAME=os.environ['HOSTNAME']
+HIPACHE_BASE_URL = 'http://%s/v1/keys%s' % (ETCD_ADDR, HIPACHE_PATH)
 
 def call(*args, **kwargs):
     kwargs['shell'] = True
@@ -15,13 +16,13 @@ def call(*args, **kwargs):
 def start():
     print " [^] Registering domain names in etcd..."
     for web_hostname in WEB_HOSTNAMES:
-        url = "http://%s/v1/keys/%s/%s/%s" % (ETCD_ADDR, HIPACHE_PATH, web_hostname, HOSTNAME)
+        url = "%s/%s/%s" % (HIPACHE_BASE_URL, web_hostname, HOSTNAME)
         call('curl -SsL "%s" -d "value=%s:%s" > /dev/null' % (url, IP_ADDR, WEB_PORT))
 
 def stop():
     print " [v] Removing domain names from etcd..."
     for web_hostname in WEB_HOSTNAMES:
-        url = "http://%s/v1/keys/%s/%s/%s" % (ETCD_ADDR, HIPACHE_PATH, web_hostname, HOSTNAME)
+        url = "%s/%s/%s" % (HIPACHE_BASE_URL, web_hostname, HOSTNAME)
         call('curl -SsL "%s" -X DELETE > /dev/null' % url)
 
 if __name__ == "__main__":
