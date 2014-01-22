c89d8a36bfbb0293d478b0a67fdc201946f06600
3onyc <3onyc@x3tech.com>
2014-01-22 22:34:19 +0100

    Combine the postgres wrapper and init script, and don't depend on arch-infra-base

--- arch-pgsql/postgres-wrapper.sh
+++ arch-pgsql/postgres-wrapper.sh
@@ -10,6 +10,17 @@ if [ ! -f "$PG_DATA/pg_hba.conf" ]; then
   echo "host all all 0.0.0.0/0 md5" >> "${PG_DATA}/pg_hba.conf"
   echo "host all all ::/0 md5" >> "${PG_DATA}/pg_hba.conf"
   echo "listen_addresses = '*'" >> "${PG_DATA}/postgresql.conf"
+
+  # Start the process of initialising the user and database
+  (
+    while ! (echo "" | /usr/bin/psql -U postgres 2>&1 > /dev/null); do sleep 1; done
+    echo "PostgreSQL Is up, initialising..."
+
+    # Create the user and database
+    echo "CREATE USER \"${PG_USER}\" WITH CREATEDB PASSWORD '${PG_PASSWORD}';" \
+         "CREATE DATABASE \"${PG_USER}_${PG_DB}\" WITH OWNER \"${PG_USER}\";" \
+         | psql
+  ) &
 fi
 
-exec /usr/bin/postgres -D "${PG_DATA}"
+exec /usr/bin/postgres -D "${PG_DATA}" "$@"
