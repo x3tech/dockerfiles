fe2324dab880953e8d27838df3fef61510aab900
3onyc <3onyc@x3tech.com>
2014-06-28 03:36:18 +0200

    [WIP][OwnCloud] Delete autconfig.tpl.php after generating config

--- owncloud/run.sh
+++ owncloud/run.sh
@@ -3,9 +3,21 @@
 readonly CONFDIR="/var/www/owncloud/config"
 
 main() {
-    envtpl < "${CONFDIR}/user.config.php.tpl" > "${CONFDIR}/user.config.php"
-    envtpl < "${CONFDIR}/autoconfig.php.tpl" > "${CONFDIR}/autoconfig.php"
+    generate_config
+    exec_init
+}
 
+generate_config() {
+    envtpl < "${CONFDIR}/user.config.php.tpl" > "${CONFDIR}/user.config.php"
+
+    # Should only be generated once, and then deleted
+    if [ -f "${CONFDIR}/autoconfig.php.tpl" ]; then
+        envtpl < "${CONFDIR}/autoconfig.php.tpl" > "${CONFDIR}/autoconfig.php"
+        rm "${CONFDIR}/autoconfig.php.tpl"
+    fi
+}
+
+exec_init() {
     exec /usr/sbin/runsvdir-start
 }
 
