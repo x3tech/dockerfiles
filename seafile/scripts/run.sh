7e5fa148bca2d078aa410f53dda7d26b376cb468
3onyc <3onyc@x3tech.com>
2014-07-08 01:39:32 +0200

    [Seafile] Add automated install scripts, and modify run.sh to rewrite topdir

--- seafile/scripts/run.sh
+++ seafile/scripts/run.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 STOP=0
 
-cd /opt/seafile/seafile-server-*
-if [ ! -e /opt/seafile/data/blocks ]; then
-    bash /modify-setup-seafile.sh
-    su seafile -c "./setup-seafile.sh"
-fi
+rewrite_topdir() {
+    find . \
+        -name "*.sh" \
+        -exec sed -E -i 's#^TOPDIR=.*#TOPDIR=/opt/seafile/data#g' {} \;
+
+    find . \
+        -name "*.py" \
+        -exec sed -E -i 's#^install_topdir =.*#install_topdir = "/opt/seafile/data"#g' {} \;
+}
+
+main() {
+    cd /opt/seafile/seafile-server-*
+    rewrite_topdir
+
+    if [ ! -e /opt/seafile/data/blocks ]; then
+        su seafile -c "./setup-seafile.sh"
+    fi
+
+    start
+    if [ "$STOP" -eq 1 ]; then
+        stop
+    fi
+
+    trap stop SIGINT SIGTERM SIGQUIT
+
+    while true; do
+        sleep .5
+    done
+}
 
 start() {
-    trap cue_stop SIGINT SIGTERM
+    trap cue_stop SIGINT SIGTERM SIGQUIT
+
     su seafile -c "./seafile.sh start"
     su seafile -c "./seahub.sh start"
 }
@@ -23,13 +48,4 @@ stop() {
     exit 0
 }
 
-start
-if [ "$STOP" -eq 1 ]; then
-    stop
-fi
-
-trap stop SIGINT SIGTERM
-
-while [ 1 ]; do
-  sleep 10
-done
+main
